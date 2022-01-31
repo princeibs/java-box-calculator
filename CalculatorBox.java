f3cf0aebe02772d56b411744b655735a4fb23bb4
Suleiman Ibrahim <suleimanonnet@gmail.com>
2022-01-31 10:29:04 +0100

    Round up the calculator logic

--- CalculatorBox.java
+++ CalculatorBox.java
@@ -2,6 +2,7 @@ package Default;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class CalculatorBox extends JFrame {
 
@@ -10,10 +11,129 @@ public class CalculatorBox extends JFrame {
     JTextField output;
     String previous, current, operator;
 
+    public void delete() {
+        if (current.length() > 0) {
+            current = current.substring(0, current.length() - 1);
+        }
+    }
+
+    public void clear() {
+        current = "";
+        previous = "";
+        operator = null;
+    }
+
+    public void updateOutput() {
+        output.setText(current);
+    }
+
+    public void appendToOutput(String num) {
+        if (num.equals(".") && current.contains(".")) {
+            return;
+        }
+        current += num;
+    }
+
+    public void selectOperator(String newOperator) {
+        // if no number is entered yet
+        if (current.isEmpty()) {
+            operator = newOperator;
+            return;
+        }
+
+        if (!previous.isEmpty()) {
+            calculate();
+        }
+
+        operator = newOperator;
+        previous = current;
+        current = "";
+    }
+
+    public void calculate() {
+        if (previous.length() < 1 || current.length() < 1) {
+            return;
+        }
+        double result = 0.0;
+        double num1 = Double.parseDouble(previous);
+        double num2 = Double.parseDouble(current);
+        switch (operator) {
+            case "*":
+                result = num1 * num2;
+                break;
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "÷":
+                result = num1 / num2;
+                break;
+            default:
+                break;
+        }
+        current = String.valueOf(result);
+        operator = null;
+        previous = "";
+    }
+
+    private class NumberBtnHandler implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JButton selectedBtn = (JButton) e.getSource();
+            for (JButton btn : numBtn) {
+                if (selectedBtn == btn) {
+                    appendToOutput(btn.getText());
+                    updateOutput();
+                }
+            }
+        }
+    }
+
+    private class OperatorBtnHandler implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JButton selectedBtn = (JButton) e.getSource();
+            if (selectedBtn == btnMultiply) {
+                selectOperator(btnMultiply.getText());
+            } else if (selectedBtn == btnAdd) {
+                selectOperator(btnAdd.getText());
+            } else if (selectedBtn == btnSubtract) {
+                selectOperator(btnSubtract.getText());
+            } else if (selectedBtn == btnDivide) {
+                selectOperator(btnDivide.getText());
+            }
+            updateOutput();
+        }
+    }
+
+    private class OtherBtnHandler implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JButton selectedBtn = (JButton) e.getSource();
+            if (selectedBtn == btnDelete) {
+                delete();
+            } else if (selectedBtn == btnClear) {
+                clear();
+            } else if (selectedBtn == btnEquals) {
+                calculate();
+            }
+            updateOutput();
+        }
+    }
+
     public CalculatorBox() {
         super("Box Calculator");
         JPanel mainPanel = new JPanel();
-        
+
+        // Initializing the calculator operands
+        current = "";
+        previous = "";
+
         // Create sub panels inside main panel
         JPanel row1 = new JPanel();
         JPanel row2 = new JPanel();
@@ -21,7 +141,7 @@ public class CalculatorBox extends JFrame {
         JPanel row4 = new JPanel();
         JPanel row5 = new JPanel();
 
- 	    // Initialize components
+        // Initialize components
         output = new JTextField(16);
         btnSubtract = new JButton("-");
         btnAdd = new JButton("+");
@@ -32,15 +152,21 @@ public class CalculatorBox extends JFrame {
         btnClear = new JButton("C");
         btnDelete = new JButton("D");
 
- 	    // Initialize, style, and add action listeners to number buttons
+        // Instantiate action listeners
+        NumberBtnHandler numBtnHandler = new NumberBtnHandler();
+        OtherBtnHandler otherBtnHandler = new OtherBtnHandler();
+        OperatorBtnHandler opBtnHandler = new OperatorBtnHandler();
+
+        // Initialize, style, and add action listeners to number buttons
         numBtn = new JButton[11];
         numBtn[10] = btnDot;
         for (int count = 0; count < numBtn.length - 1; count++) {
             numBtn[count] = new JButton(String.valueOf(count));
             numBtn[count].setFont(new Font("Monospaced", Font.BOLD, 22));
+            numBtn[count].addActionListener(numBtnHandler);
         }
 
- 	    // Style other buttons
+        // Style other buttons
         btnDot.setFont(new Font("Monospaced", Font.BOLD, 22));
         btnEquals.setFont(new Font("Monospaced", Font.BOLD, 22));
         btnAdd.setFont(new Font("Monospaced", Font.BOLD, 22));
@@ -49,21 +175,33 @@ public class CalculatorBox extends JFrame {
         btnMultiply.setFont(new Font("Monospaced", Font.BOLD, 22));
         btnClear.setFont(new Font("Monospaced", Font.BOLD, 20));
         btnDelete.setFont(new Font("Monospaced", Font.BOLD, 20));
-        
+
         // Style the output display
         output.setMaximumSize(new Dimension(185, 40));
         output.setFont(new Font("Monospaced", Font.BOLD, 27));
         output.setDisabledTextColor(new Color(0, 0, 0));
         output.setMargin(new Insets(0, 5, 0, 0));
-	    output.setText("0");
-        
+        output.setText("0");
+
+        // Add action listeners to other buttons
+        btnDot.addActionListener(numBtnHandler);
+        btnDelete.addActionListener(otherBtnHandler);
+        btnClear.addActionListener(otherBtnHandler);
+        btnEquals.addActionListener(otherBtnHandler);
+
+        // Add action listeners to operation buttons
+        btnMultiply.addActionListener(opBtnHandler);
+        btnAdd.addActionListener(opBtnHandler);
+        btnSubtract.addActionListener(opBtnHandler);
+        btnDivide.addActionListener(opBtnHandler);
+
         // Set the layout of each row in the pane
         row1.setLayout(new BoxLayout(row1, BoxLayout.LINE_AXIS));
         row2.setLayout(new BoxLayout(row2, BoxLayout.LINE_AXIS));
         row3.setLayout(new BoxLayout(row3, BoxLayout.LINE_AXIS));
         row4.setLayout(new BoxLayout(row4, BoxLayout.LINE_AXIS));
         row5.setLayout(new BoxLayout(row5, BoxLayout.LINE_AXIS));
-        
+
         // Add components to each of the row
         row1.add(Box.createHorizontalGlue());
         row1.add(btnClear);
@@ -77,14 +215,14 @@ public class CalculatorBox extends JFrame {
         row3.add(numBtn[6]);
         row3.add(btnAdd);
         row4.add(numBtn[1]);
-        row4.add(numBtn[2]);
+        row4.add(numBtn[2]);;
         row4.add(numBtn[3]);
         row4.add(btnSubtract);
         row5.add(btnDot);
         row5.add(numBtn[0]);
         row5.add(btnEquals);
         row5.add(btnDivide);
-        
+
         // Add all rows to the main panel
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
         mainPanel.add(output);
@@ -94,7 +232,7 @@ public class CalculatorBox extends JFrame {
         mainPanel.add(row3);
         mainPanel.add(row4);
         mainPanel.add(row5);
-        
+
         // final touch
         this.add(mainPanel);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
